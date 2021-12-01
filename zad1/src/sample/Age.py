024ca84b47e50cb69c7c78d43d59c1815faddf45
Natalia Sobisz <nata.so@wp.pl>
2021-12-01 01:48:57 +0100

    zad3

--- zad1/src/sample/Age.py
+++ zad1/src/sample/Age.py
@@ -4,7 +4,7 @@ class Age:
         if type(word) != str and type(number) != int:
             raise Exception("Błąd, argumenty są złego typu danych")
         else:
-                a = int(number)
+                a = number
                 b = word.lower()
                 if a < 0:
                     raise Exception("Błąd, drugi argument musi być większy od zera")
@@ -13,23 +13,22 @@ class Age:
                 else:
                     p = 31557600
                     if b == "ziemia":
-                        return (round(a / p, 2))
+                        return round(a / p, 2)
                     elif b == "merkury":
-                        return (round(a / (p * 0.2408467), 2))
+                        return round(a / (p * 0.2408467), 2)
                     elif b == "wenus":
-                        return (round(a / (p * 0.61519726), 2))
+                        return round(a / (p * 0.61519726), 2)
                     elif b == "mars":
-                        return (round(a / (p * 1.8808158), 2))
+                        return round(a / (p * 1.8808158), 2)
                     elif b == "jowisz":
-                        return (round(a / (p * 11.862615), 2))
+                        return round(a / (p * 11.862615), 2)
                     elif b == "saturn":
-                        return (round(a / (p * 29.447498), 2))
+                        return round(a / (p * 29.447498), 2)
                     elif b == "uran":
-                        return (round(a / (p * 84.016846), 2))
+                        return round(a / (p * 84.016846), 2)
                     elif b == "neptun":
-                        return (round(a / (p * 164.79132), 2))
+                        return round(a / (p * 164.79132), 2)
 
 
 
 
-print(Age().how_old(890245364,"uran"))
